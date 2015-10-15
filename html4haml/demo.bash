38d7ec9f43f59f37246453b9e1065285c89a05b4
Dan Bikle <dan.bikle@gmail.com>
2015-10-15 23:37:07 +0000

    mo

--- html4haml/demo.bash
+++ html4haml/demo.bash
@@ -5,6 +5,6 @@
 cd ~ann/app21/html4haml/
 
 . ~ann/app21/app21env.bash
-~ann/app21/gems/bin/html2haml --html-attributes ~ann/app21/html4haml/demo10.html ~ann/app21/haml/temp.haml
+~ann/app21/gems/bin/html2haml --html-attributes ~ann/app21/html4haml/demo30.html ~ann/app21/haml/temp.haml
 
 exit
